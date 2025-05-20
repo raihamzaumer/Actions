0a8680d8e1d541a6b9c1f293ca6805a667df0b40
Hamza Umer <raihamza925@gmail.com>
2025-05-20 16:50:56 +0500

    flag

--- .github/workflows/ascii-artwork.sh
+++ .github/workflows/ascii-artwork.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
  sudo apt-get install -y figlet
- echo cowsay 
  figlet  "Run for cover, I am a DRAGON......RAWR" >> dragon.txt
  cat dragon.txt
  ls -ltra
+ echo -e "\e[42m                      \e[0m"
+ echo -e "\e[42m   \e[107m ☪︎  ✦       \e[42m   \e[0m"
+ echo -e "\e[42m                      \e[0m"
+
+
